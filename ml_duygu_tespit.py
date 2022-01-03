cf49029093af8fcc12e4509263f688865bee440b
MakinaFleo <83438309+makinafleo@users.noreply.github.com>
2022-01-03 07:45:44 +0300

    Add files via upload

---
+++ ml_duygu_tespit.py
@@ -0,0 +1,151 @@
+from gtts import gTTS
+from playsound import playsound
+import time
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+import cv2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+ap = argparse.ArgumentParser()
+ap.add_argument("--mode",help="train/display")
+mode = ap.parse_args().mode
+
+# grafik çıkartmak için
+def plot_model_history(model_history):
+
+    fig, axs = plt.subplots(1,2,figsize=(15,5))
+    # summarize history for accuracy
+    axs[0].plot(range(1,len(model_history.history['accuracy'])+1),model_history.history['accuracy'])
+    axs[0].plot(range(1,len(model_history.history['val_accuracy'])+1),model_history.history['val_accuracy'])
+    axs[0].set_title('Model Accuracy')
+    axs[0].set_ylabel('Accuracy')
+    axs[0].set_xlabel('Epoch')
+    axs[0].set_xticks(np.arange(1,len(model_history.history['accuracy'])+1),len(model_history.history['accuracy'])/10)
+    axs[0].legend(['train', 'val'], loc='best')
+    # loss histogramı
+    axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
+    axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
+    axs[1].set_title('Model Loss')
+    axs[1].set_ylabel('Loss')
+    axs[1].set_xlabel('Epoch')
+    axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
+    axs[1].legend(['train', 'val'], loc='best')
+    fig.savefig('plot.png')
+    plt.show()
+
+# dosyalar
+train_dir = 'data/train'
+val_dir = 'data/test'
+
+num_train = 28709
+num_val = 7178
+batch_size = 64
+num_epoch = 200
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(48,48),
+        batch_size=batch_size,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+validation_generator = val_datagen.flow_from_directory(
+        val_dir,
+        target_size=(48,48),
+        batch_size=batch_size,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+# model oluşturmak
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+
+# modelimiz eğiymek için
+if mode == "train":
+    model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
+    model_info = model.fit_generator(
+            train_generator,
+            steps_per_epoch=num_train // batch_size,
+            epochs=num_epoch,
+            validation_data=validation_generator,
+            validation_steps=num_val // batch_size)
+    plot_model_history(model_info)
+    model.save_weights('model.h5')
+
+# kamera ile duygu tespiti  
+elif mode == "display":
+    model.load_weights('model.h5')
+    cv2.ocl.setUseOpenCL(False)
+    emotion_dict = {0: "Sinirli", 1: "igrenmis", 2: "korkulu", 3: "Mutlu", 4: "Dogal", 5: "Uzgun", 6: "Sasirmis"}
+    cap = cv2.VideoCapture(0)
+    while True:
+        # yüz buluyor
+        ret, frame = cap.read()
+        if not ret:
+            break
+        facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+            prediction = model.predict(cropped_img)
+            maxindex = int(np.argmax(prediction))
+            cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+            '''if maxindex == 0:
+                #time.sleep(1)
+                #playsound('sinirli.mp3')
+            elif maxindex == 1:
+                #time.sleep(1)
+                #playsound('igrenmis.mp3')
+            elif maxindex == 2:
+                #time.sleep(1)
+                #playsound('korku.mp3')
+            elif maxindex == 3:
+                #time.sleep(1)
+                #playsound('mutlu.mp3')
+            elif maxindex == 4:
+                #time.sleep(1)
+                #playsound('dogal.mp3')
+            elif maxindex == 5:
+                #time.sleep(1)
+                #playsound('uzgun.mp3')
+            elif maxindex == 6:
+                #time.sleep(1)
+                #playsound('sasirmis.mp3')
+            else:
+                #print(maxindex)'''
+        cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
